d83ebbdabc826473606885af64d44740eb452243
JAVERIAADIL <50518264+JAVERIAADIL@users.noreply.github.com>
2022-01-16 02:31:21 +0500

    Update create_dependents.php

--- create_dependents.php
+++ create_dependents.php
@@ -1,5 +1,5 @@
 <?php include('connections.php');
-include('header.php');
+
 error_reporting(0); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,34 +7,75 @@ error_reporting(0); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="form.css">
     <title>Health Metrics | Create Dependents</title>
 </head>
 <body>
-    <div class="div-center">
-            <div class="content">
-    <center>
-        <h1>Create Dependents</h1>
-    </center>
-
-   <form action="" method="post">
-        <div class="form-group"> <label for=""> Employee ID </label> <input type="number" name="dempid" value="<?php echo $eid ?>" disabled /></div>
-        <div class="form-group"> <label for=""> First Name </label> <input type="text" name="fname" /> e.g. Muhammad </div>
-        <div class="form-group"> <label for=""> Last Name </label><input type="text" name="lname" /> e.g. Jan </div>
-       <div class="form-group"> <label class="form-label" for="">Gender:</label>  
-        <div class="form-check"><label class="form-check-label" for=""> Male</label>
-        <input class="form-check-input" type="radio" name="gender" value="Male" /></div>
-        <div class="form-check"><label class="form-check-label" for=""> Female </label> 
-        <input class="form-check-input" type="radio" name="gender" value="Female"/></div>
+<div class="container">
+    <div class="title">Create Dependents</div>
+    <div class="content">
+    <form action="" method="post">
+        <div class="user-details">
+        <div class="input-box">
+            <span class="details">Employee ID</span>
+            <input type="number" name="dempid" placeholder="Enter your respective Employee Id" value="<?php echo $data['EMPID'] ?>"  required />
+          </div>
+          <div class="box">
+          <div class="input-box">
+            <span class="details">First Name </span>
+            <input type="text" placeholder="Enter your first name" name="fname" required> e.g. Muhammad
+          </div>
+          <div class="input-box">
+            <span class="details">Last Name </span>
+            <input type="text" placeholder="Enter your last name" name="lname" required> e.g. Jan
+          </div>
+         
+         
+          </div>
+          
         </div>
-        <div class="form-group"> <label for=""> CNIC </label> <input type="number" name="cnic" /> XXXXXXXXXXXXX </div>
-        <div class="form-group"> <label for=""> DOB </label> <input type=text placeholder="YYYY-MM-DD" name="dob"/> e.g. 2001-02-25 </div>
-        <input class="btn btn-primary" type="submit" name="submit" value="Submit"/>
-    </form>
+        <div class="gender-details">
+          <input type="radio" name="gender" id="dot-1">
+          <input type="radio" name="gender" id="dot-2">
+          <span class="gender-title">Gender</span>
+          <div class="category">
+            <label for="dot-1">
+            <span class="dot one"></span>
+            <span class="gender">Male</span>
+          </label>
+          <label for="dot-2">
+            <span class="dot two"></span>
+            <span class="gender">Female</span>
+          </label>
+         
+          </div>
+        
+        <div class="input-box">
+            <span class="details">CNIC Number</span>
+            <input type="number" name="cnic" placeholder="Enter your CNIC" required> XXXXXXXXXXXXX
+          </div>
+          <div class="input-box">
+            <span class="details">Date Of Birth</span>
+            <input type="text" placeholder="YYYY-MM-DD" name="dob" required> e.g. 2001-02-25 
+          </div>
+
+        <div class="button">
+          <input type="submit" value="Submit" name="submit">
+        </div>
+      </form>
     </div>
-    </div>
-     <?php 
+  </div>
+<!-- 
+    <form action="" method="post">
+        Employee ID <input type="number" name="dempid" value="/* #?php echo $eid ?>/" disabled /><br><br>
+        First Name <input type="text" name="fname" /> e.g. Muhammad <br><br>
+        Last Name <input type="text" name="lname" /> e.g. Jan <br><br>
+        Gender:   Male <input type="radio" name="gender" value="Male" /> Female <input type="radio" name="gender" value="Female"/> <br><br>
+        CNIC <input type="number" name="cnic" /> XXXXXXXXXXXXX <br><br>
+        DOB <input type=text placeholder="YYYY-MM-DD" name="dob"/> e.g. 2001-02-25 <br><br>
+        <input type="submit" name="submit" value="Submit"/>
+    </form> -->
+    <?php 
     $eid=$_GET['id'];
     ?>
     <?php
@@ -59,9 +100,7 @@ error_reporting(0); ?>
         echo 'Data not Inserted :(';
     }
     }
-    else{
-        echo 'All Fields are required!';
-    }
+    
 
     ?>
     
