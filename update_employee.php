9fa008fec7bbd849f1b6a4689e001224313e680e
JAVERIAADIL <50518264+JAVERIAADIL@users.noreply.github.com>
2022-01-16 02:58:16 +0500

    Update update_employee.php

--- update_employee.php
+++ update_employee.php
@@ -1,5 +1,5 @@
 <?php include('connections.php');
-include('header.php');
+
 error_reporting(0); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,13 +7,12 @@ error_reporting(0); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+    <title>Health Metrics | Update Employee</title>
 </head>
 <body>
     
-<center>
-        <h1>Update Employee</h1>
-    </center>
+
     
     <?php 
     $eid=$_GET['id'];
@@ -22,6 +21,80 @@ error_reporting(0); ?>
     if(mysqli_num_rows($result_select)>0){
         while($data=mysqli_fetch_assoc($result_select)){
     ?>
+ <div class="container">
+    <div class="title"> <center>Update Employee</center></div>
+    <div class="content">
+    
+     <form action="" method="post">
+        <div class="user-details">
+        <div class="input-box">
+            <span class="details">Doctor ID</span>
+            <input type="hidden" name="e_id"  value=" <?php echo $data['EMPID'] ?>"  />
+          </div>
+         
+        
+    <div class="input-box">
+            <span class="details">First Name </span>
+            <input type="text" name="fname" value=" <?php echo $data['EMP_FNAME'] ?>"  /> e.g. Muhammad
+          </div>
+          <div class="input-box">
+            <span class="details">Last Name </span>
+            <input type="text" name="lname" value="<?php echo $data['EMP_LNAME'] ?>"/> e.g. Jan
+          </div>
+        
+          </div>
+        <div class="gender-details">
+          <input type="radio" name="gender" id="dot-1" <?php if($data['GENDER'] == "Male"){echo "checked"; $dgender="Male";}?> value="Male" />
+          <input type="radio" name="gender" id="dot-2" <?php if($data['GENDER'] == "Female"){echo "checked"; $dgender="Female";}?> value="Female"/>
+          
+          <span class="gender-title">Gender</span>
+          <div class="category">
+            <label for="dot-1">
+            <span class="dot one"></span>
+            <span class="gender">Male</span>
+          </label>
+          <label for="dot-2">
+            <span class="dot two"></span>
+            <span class="gender">Female</span>
+          </label>
+         
+          </div>
+        </div>
+        <div class="input-box">
+            <span class="details">Email</span>
+            <input type="email" name="email" value=" <?php echo $data['EMP_EMAIL'] ?>" /> e.g. abc_123@xyz.com
+          </div>
+          <div class="input-box">
+            <span class="details">Qualification</span>
+            <input type="text" name="designation" value=" <?php echo $data['DESIGNATION'] ?>" />e.g. Director
+          </div>
+          <div class="input-box">
+            <span class="details">CNIC Number</span>
+            <input type="number" name="contact" value=" <?php echo $data['EMP_CONTACT'] ?>" />e.g. 03XXXXXXXXX
+          </div>
+        <div class="input-box">
+            <span class="details">CNIC Number</span>
+            <input type="number" name="cnic" value=" <?php echo $data['EMP_CNIC'] ?>" /> XXXXXXXXXXXXX
+          </div>
+          <div class="input-box">
+            <span class="details">Date Of Birth</span>
+            <input type="text" placeholder="YYYY-MM-DD" name="dob"  value="<?php echo $data['DOB'] ?>"> e.g. 2001-02-25 
+          </div>
+         
+        <div class="button">
+          <input type="submit" value="Submit" name="submit">
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
 <form action="" method="post">
         <input type="hidden" name="e_id" value="<?php echo $data['EMPID'] ?>"/><br>
         First Name <input type="text" name="fname" value="<?php echo $data['EMP_FNAME'] ?>" /> e.g. Muhammad <br>
@@ -53,7 +126,7 @@ error_reporting(0); ?>
         echo 'Data Updated Successfully :)';
         #header("Location: http://localhost/testpro/read_employee.php");
         
-        echo "<script>window.top.location='http://localhost/testpro/read_employee.php'</script>";
+        echo "<script>window.top.location='http://localhost/Health_Metrics/read_employee.php'</script>";
     }
     else{
         echo 'Data not Updated :(';
