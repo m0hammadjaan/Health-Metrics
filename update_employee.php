0c68c55359aceb3847ec292c8f124e88e97aa93f
JAVERIAADIL <50518264+JAVERIAADIL@users.noreply.github.com>
2022-01-16 13:12:04 +0500

    Update update_employee.php

--- update_employee.php
+++ update_employee.php
@@ -14,15 +14,9 @@ error_reporting(0); ?>
     
 
     
-    <?php 
-    $eid=$_GET['id'];
-    $sql_select="select * from employee where EMPID={$eid}";
-    $result_select=mysqli_query($conn,$sql_select);
-    if(mysqli_num_rows($result_select)>0){
-        while($data=mysqli_fetch_assoc($result_select)){
-    ?>
+   
  <div class="container">
-    <div class="title"> <center>Update Employee</center></div>
+    <div class="title"> Update Employee</div>
     <div class="content">
     
      <form action="" method="post">
@@ -90,23 +84,17 @@ error_reporting(0); ?>
 
 
 
+  <?php 
+    $eid=$_GET['id'];
+    $sql_select="select * from employee where EMPID={$eid}";
+    $result_select=mysqli_query($conn,$sql_select);
+    if(mysqli_num_rows($result_select)>0){
+        while($data=mysqli_fetch_assoc($result_select)){
+    ?>
 
 
 
 
-
-<form action="" method="post">
-        <input type="hidden" name="e_id" value="<?php echo $data['EMPID'] ?>"/><br>
-        First Name <input type="text" name="fname" value="<?php echo $data['EMP_FNAME'] ?>" /> e.g. Muhammad <br>
-        Last Name <input type="text" name="lname" value="<?php echo $data['EMP_LNAME'] ?>"/> e.g. Jan <br>
-        Gender:   Male <input type="radio" name="gender" <?php if($data['GENDER'] == "Male"){echo "checked"; $egender="Male";}?> value="Male" /> Female <input type="radio" name="gender" <?php if($data['GENDER'] == "Female"){echo "checked"; $egender="Female";}?> value="Female" /> <br>
-        Email <input type="email" name="email" value="<?php echo $data['EMP_EMAIL'] ?>"/> e.g. abc_123@xyz.com <br>
-        Designation <input type="text" name="designation" value="<?php echo $data['DESIGNATION'] ?>" /> e.g. Director <br>
-        Contact <input type="number" name="contact" value="<?php echo $data['EMP_CONTACT'] ?>" /> e.g. 03XXXXXXXXX <br>
-        CNIC <input type="number" name="cnic" value="<?php echo $data['EMP_CNIC'] ?>" /> XXXXXXXXXXXXX <br>
-        DOB <input type=text placeholder="YYYY-MM-DD" name="dob" value="<?php echo $data['DOB'] ?>"/> e.g. 2001-02-25 <br>
-        <input type="submit" name="submit" value="Update"/>
-    </form>
     <?php }
     }
     $efname = $_POST['fname'];
