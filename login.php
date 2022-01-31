5aa2a6060bc0a471d5f1a29fd073ba4764189840
Muhammad Jan <janmuhammad2050@gmail.com>
2022-01-31 17:14:53 +0500

    Muhammad Jan

--- login.php
+++ login.php
@@ -10,8 +10,6 @@ error_reporting(0);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-     
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Health Metrics | Login</title>
 </head>
